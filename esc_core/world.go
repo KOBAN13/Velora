602a0e3ca1b64b273dd4cde9e685f176bb9d8746
Daniil <layk.danil@mail.ru>
2026-06-18 23:49:42 +0400

    WIP Build esc core

--- esc_core/world.go
+++ esc_core/world.go
@@ -1,5 +1,12 @@
 package esc_core
 
+import (
+	"fmt"
+	"reflect"
+)
+
+type WorldOption func(*World)
+
 type World struct {
 	slots             []entitySlot
 	freeEntityIndexes []uint32
@@ -8,6 +15,12 @@ type World struct {
 	registry          *ComponentRegistry
 	mutationPhase     MutationPhase
 	archetypeVersion  uint64
+}
+
+func WithEntityCapacity(capacity int) WorldOption {
+	return func(world *World) {
+		world.slots = make([]entitySlot, 0, capacity)
+	}
 }
 
 func NewWorld(options ...WorldOption) *World {
@@ -21,6 +34,10 @@ func NewWorld(options ...WorldOption) *World {
 	}
 
 	return world
+}
+
+func Spawn(world *World, components ...any) (Entity, error) {
+	var entity Entity
 }
 
 func (world *World) allocateEntity() Entity {
@@ -43,4 +60,53 @@ func (world *World) releaseEntity(entity Entity) {
 	slot.location = entityLocation{}
 	slot.generation++
 	world.freeEntityIndexes = append(world.freeEntityIndexes, entity.index)
+}
+
+func (world *World) validateAlive(entity Entity) (*entitySlot, error) {
+
+}
+
+func (world *World) collectComponentValues(components []any) (map[ComponentID]any, componentSignature, error) {
+	var values = make(map[ComponentID]any, len(components))
+	var ids = make([]ComponentID, 0, len(components))
+
+	for _, component := range components {
+		if component == nil {
+			return nil, componentSignature{}, ErrInvalidComponentType
+		}
+
+		var token = ComponentToken{
+			Type: reflect.TypeOf(component),
+			Name: reflect.TypeOf(component).Name(),
+		}
+
+		var info, err = world.registry.Info(token)
+
+		if err != nil {
+			return nil, componentSignature{}, err
+		}
+
+		if _, exists := values[info.Id]; exists {
+			return nil, componentSignature{}, fmt.Errorf("%w: %s", ErrDuplicateComponent, info.Name)
+		}
+
+		values[info.Id] = component
+		ids = append(ids, info.Id)
+	}
+
+	var signature = newComponentSignature(ids)
+
+	return values, *signature, nil
+}
+
+func (world *World) archetypeFor(signature componentSignature) (*archetype, error) {
+
+}
+
+func (world *World) moveEntity(entity Entity, target *archetype, values map[ComponentID]any) error {
+
+}
+
+func (world *World) removeFromCurrentArchetype(entity Entity, slot *entitySlot) {
+
 }
